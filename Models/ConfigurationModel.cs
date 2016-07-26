6bcd76d6d5612ac8632a0ebd15268df5e2d9de08
rafal kalisz <rafal.kalisz.dev@gmail.com>
2016-07-26 16:52:29 +0200

    init

---
+++ Models/ConfigurationModel.cs
@@ -0,0 +1,22 @@
+﻿using Grand.Web.Framework;
+using Grand.Web.Framework.Mvc;
+
+namespace Grand.Plugin.Payments.PurchaseOrder.Models
+{
+    public class ConfigurationModel : BaseNopModel
+    {
+        public string ActiveStoreScopeConfiguration { get; set; }
+
+        [NopResourceDisplayName("Plugins.Payment.PurchaseOrder.AdditionalFee")]
+        public decimal AdditionalFee { get; set; }
+        public bool AdditionalFee_OverrideForStore { get; set; }
+
+        [NopResourceDisplayName("Plugins.Payment.PurchaseOrder.AdditionalFeePercentage")]
+        public bool AdditionalFeePercentage { get; set; }
+        public bool AdditionalFeePercentage_OverrideForStore { get; set; }
+
+        [NopResourceDisplayName("Plugins.Payment.PurchaseOrder.ShippableProductRequired")]
+        public bool ShippableProductRequired { get; set; }
+        public bool ShippableProductRequired_OverrideForStore { get; set; }
+    }
+}
